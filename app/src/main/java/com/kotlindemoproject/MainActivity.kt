28fd26f15d5cab25b41bb6be5f492e652e8f2f17
chetan2991 <chetanshingare2991@gmail.com>
2017-04-18 18:40:51 +0530

    first commit

---
+++ app/src/main/java/com/kotlindemoproject/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.kotlindemoproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity()
+{
+    override
+    fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun printTheDemo()
+    {
+        Log.d("ram", "sham")
+    }
+}
